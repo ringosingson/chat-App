8a449bd5dc5e5ff8d62b7de560c32bdac887535b
Ringo Singson <ringosingson@live.com>
2019-04-30 02:47:15 +0530

    sstoring data in firebase

--- src/ChatInputBox.js
+++ src/ChatInputBox.js
@@ -1,10 +1,25 @@
-import React from 'react';
+import React from "react";
+import { db } from "./firebase";
 
 function ChatInputBox() {
   return (
-    <div className="ChatInputBox">
+    <form
+      onSubmit={event => {
+        event.preventDefault();
+        const value = event.target.elements[0].value;
+        db.collection("channels")
+          .doc("general")
+          .collection("messages")
+          .add({
+            text: value,
+            createdAt: new Date()
+          });
+        event.target.reset();
+      }}
+      className="ChatInputBox"
+    >
       <input className="ChatInput" placeholder="Message #general" />
-    </div>
+    </form>
   );
 }
 
