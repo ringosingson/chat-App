349fd1987024646403312999341439b6a2a255b6
Ringo Singson <ringosingson@live.com>
2019-05-02 04:20:29 +0530

    deploy test

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,35 @@
-const functions = require('firebase-functions');
+const functions = require("firebase-functions");
+const admin = require("firebase-admin");
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+admin.initializeApp();
+
+const db = admin.firestore();
+
+exports.onUserStatusChanged = functions.database
+  .ref("status/{userId}")
+  .onUpdate((change, context) => {
+    const eventStatus = change.after.val();
+    const userDoc = db.doc(`users/${context.params.userId}`);
+    return change.after.ref.once("value").then(snapshot => {
+      const status = snapshot.val();
+      if (status.lastChanged > eventStatus.lastChanged) {
+        return;
+      }
+      eventStatus.lastChanged = new Date(eventStatus.lastChanged);
+      userDoc.update({
+        status: eventStatus
+      });
+    });
+  });
+
+exports.helloWorld = functions.https.onRequest((request, response) => {
+  console.log("Hello there!");
+  if (Math.random() > 0.25) {
+    console.error("Blow up!");
+  } else if (Math.random() > 0.25) {
+    throw new Error("hey!!");
+  } else {
+    console.log("hey u");
+  }
+  response.send("Hello with errors and stuff!");
+});
